4b04fe35f83c8fc32c0fb245cff4d59a46c8d73d
muskan <muski.kool@gmail.com>
2020-07-15 23:40:03 +0530

    First Commit

--- House_Cost/src/main/java/project/House_Cost/Construction.java
+++ House_Cost/src/main/java/project/House_Cost/Construction.java
@@ -1,8 +1,12 @@
 package project.House_Cost;
+import Main;
 import java.io.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.*;
 public class Construction
 	{
+	private static final jdk.internal.instrumentation.Logger LOGGER = LogManager.getLogger(Construction.class);
 	public void estimation() throws IOException
 	   {
 	        String standard,automation ;
@@ -10,35 +14,35 @@ public class Construction
 	        
 	        String type1="standard material",type2="above standard material",type3="highly standard material",automation1="no",automation2="yes";
 	        Scanner sc = new Scanner (System.in);
-	        System.out.write("enter the material of the house (standard material/above standard material/highly standard material)\n".getBytes());
+	        LOGGER.info("enter the material of the house (standard material/above standard material/highly standard material)\n".getBytes());
 	        standard = sc.nextLine();
-	        System.out.write("enter whether the house is fully automated(yes/no) : \n".getBytes());
+	        LOGGER.info("enter whether the house is fully automated(yes/no) : \n".getBytes());
 	        automation=sc.nextLine();
-	        System.out.write("enter the area of the house : \n".getBytes());
+	        LOGGER.info("enter the area of the house : \n".getBytes());
 	        area=sc.nextDouble();
 	        if(standard.compareTo(type1)==0 && automation.compareTo(automation1)==0)
 	        {
 	            estimate=area*1200;
-	            System.out.printf("The estimated cost is : %f \n",estimate);
+	            LOGGER.debug("The estimated cost is : %f \n",estimate);
 	        }
 	        else if(standard.compareTo(type2)==0 && automation.compareTo(automation1)==0)
 	        {
 	            estimate=area*1500;
-	            System.out.printf("The estimated cost is : %f \n",estimate);
+	            LOGGER.debug("The estimated cost is : %f \n",estimate);
 	        }
 	        else if(standard.compareTo(type3)==0 && automation.compareTo(automation1)==0)
 	        {
 	            estimate=area*1800;
-	            System.out.printf("The estimated cost is : %f \n",estimate);
+	            LOGGER.debug("The estimated cost is : %f \n",estimate);
 	        }
 	        else if(standard.compareTo(type3)==0 && automation.compareTo(automation2)==0)
 	        {
 	            estimate=area*2500;
-	            System.out.printf("The estimated cost is : %f \n",estimate);
+	            LOGGER.debug("The estimated cost is : %f \n",estimate);
 	        }
 	        else
 	            {
-	                System.out.printf("wrong input : ");
+	                LOGGER.error("!!!Please Enter Correct Inputs!!!");
 	            }
 	    }
 
