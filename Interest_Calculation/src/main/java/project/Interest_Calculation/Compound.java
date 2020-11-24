6baaa12019bdd088b19fc6391cbeb679a78be248
Muskan Salampuria <muski.kool@gmail.com>
2020-11-24 20:49:05 +0530

    Update Compound.java

--- Interest_Calculation/src/main/java/project/Interest_Calculation/Compound.java
+++ Interest_Calculation/src/main/java/project/Interest_Calculation/Compound.java
@@ -4,7 +4,6 @@ import java.lang.Math;
 public class Compound extends Interest{
 	public float calc_Interest(float amount,float rate_of_interest,int time,int compounded)
 	{
-		 return(amount*(float)(Math.pow(1+(rate_of_interest/(100*compounded)),compounded*time)));
-		
+		 return(amount*(float)(Math.pow(1+(rate_of_interest/(100*compounded)),compounded*time)));	
 	}
 }
