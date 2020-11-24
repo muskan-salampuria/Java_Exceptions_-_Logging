20263d5141d6a96f92c508837b469b6c35cb6234
Muskan Salampuria <muski.kool@gmail.com>
2020-11-24 20:47:47 +0530

    Update Simple.java

--- Interest_Calculation/src/main/java/project/Interest_Calculation/Simple.java
+++ Interest_Calculation/src/main/java/project/Interest_Calculation/Simple.java
@@ -3,7 +3,6 @@ package project.Interest_Calculation;
 public class Simple extends Interest{
 	public float calc_Interest(float amount,float rate_of_interest,int time,int compounded)
 	{
-		 return(amount*(1+(rate_of_interest/100)*time));
-		
+		 return(amount*(1+(rate_of_interest/100)*time));	
 	}
 }
